0105ef6aeeda49c5683289cea5605b2b6df50d04
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:51:42 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/libpng/build-linux32.sh
+++ csrc/libpng/build-linux32.sh
@@ -1,1 +1,1 @@
-gcc $CFLAGS *.c -shared -o ../../bin/linux32/libpng.so -I../zlib -L../../bin/linux32 -lz
+gcc -O2 -s -static-libgcc *.c -shared -o ../../bin/linux32/libpng.so -I../zlib -L../../bin/linux32 -lz
--- csrc/libpng/build-mingw32.sh
+++ csrc/libpng/build-mingw32.sh
@@ -1,1 +1,1 @@
-gcc $CFLAGS *.c -shared -o ../../bin/mingw32/png.dll -I../zlib -L../../bin/mingw32 -lz
+gcc -O2 -s -static-libgcc *.c -shared -o ../../bin/mingw32/png.dll -I../zlib -L../../bin/mingw32 -lz
