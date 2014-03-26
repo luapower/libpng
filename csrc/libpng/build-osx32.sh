f85498a75dc73b42d545f0d9176c2e2a8abc088a
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:08:14 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/libpng/build-osx32.sh
+++ csrc/libpng/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 *.c -shared -o ../../bin/osx32/libpng.dylib -I../zlib -L../../bin/osx32 -lz
+gcc -arch i386 -O2 *.c -shared -install_name @loader_path/libpng.dylib -o ../../bin/osx32/libpng.dylib -I../zlib -L../../bin/osx32 -lz
--- csrc/libpng/build-osx64.sh
+++ csrc/libpng/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 *.c -shared -o ../../bin/osx64/libpng.dylib -I../zlib -L../../bin/osx64 -lz
+gcc -arch x86_64 -O2 *.c -shared -install_name @loader_path/libpng.dylib -o ../../bin/osx64/libpng.dylib -I../zlib -L../../bin/osx64 -lz
