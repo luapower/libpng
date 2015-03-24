a78cab34fff8d69b66425e3870c454354650da2d
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 12:59:14 +0200

    unimportant

--- csrc/libpng/build-linux64.sh
+++ csrc/libpng/build-linux64.sh
@@ -1,1 +1,1 @@
-P=linux64 C=-fPIC L="-s -static-libgcc" D=libpng.so A=libpng.a ./build.sh
+P=linux64 C="-fPIC -include _memcpy.h -U_FORTIFY_SOURCE" L="-s -static-libgcc" D=libpng.so A=libpng.a ./build.sh
